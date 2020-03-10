4258c285c645e696b7686702b9475e172a493c91
Oliver <Oliver@ideaY510P-NB>
2020-03-10 19:53:32 +0100

    MysqlAdminDAOTest
    commented and added tests to MysqlAdminDAOTest class.

--- LabBook/src/test/java/vahovsky/LabBook/test/persistent/MysqlAdminDAOTest.java
+++ LabBook/src/test/java/vahovsky/LabBook/test/persistent/MysqlAdminDAOTest.java
@@ -7,18 +7,32 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import vahovsky.LabBook.entities.Admin;
+import vahovsky.LabBook.entities.Entity;
 import vahovsky.LabBook.persistent.AdminDAO;
 import vahovsky.LabBook.persistent.DAOfactory;
 
 class MysqlAdminDAOTest {
 
+	/**
+	 * Method that tests method <vode>getAll()</code> in class
+	 * <code>MysqlAdminDAO</code>. Tests if the list of admin returned from the
+	 * database have size > 0.
+	 */
 	@Test
 	void testGetAll() {
 		List<Admin> admins = DAOfactory.INSTANCE.getAdminDAO().getAll();
 		assertNotNull(admins);
 		assertTrue(admins.size() > 0);
 	}
-	
+
+	/**
+	 * Method that tests method <code>addAdmin(Admin admin)</code> and
+	 * <code>deleteEntity(Entity admin)</code> in a class
+	 * <code>MysqlAdminDAO</code>. Test admin is created and it is checked if such
+	 * an admin is not already in the database. He is then added into the database
+	 * and it is tested if the addition was successful. In the end he is removed
+	 * from the database and it is tested if the removal was successful.
+	 */
 	@Test
 	void addDeleteTest() {
 		Admin testAdmin = new Admin();
@@ -54,7 +68,14 @@ class MysqlAdminDAOTest {
 		}
 		assertTrue(successfullyDeleted);
 	}
-	
+
+	/**
+	 * Method that tests method <code>saveAdmin</code> in class
+	 * <code>MysqlAdminDAO</code>. Test admin is made and saved in to database.
+	 * First test runs to assert that he was correctly added into the database. Next
+	 * some of his parameters are changed and saved. Then we look for the test admin
+	 * in the database (through his ID) and check, if his parameters really changed.
+	 */
 	@Test
 	void testSave() {
 		Admin testAdmin = new Admin();
@@ -77,6 +98,26 @@ class MysqlAdminDAOTest {
 			}
 		}
 		assertTrue(false, "update sa nepodaril");
+	}
+
+	/**
+	 * Method that tests method <code>isNameAvailable(String name)</code> in class
+	 * <code>MysqlAdminDAO</code>. Test admin is made and added into the database.
+	 * Before the addition, the name of the test admin should not by present in the
+	 * database, hence method <code>isNameAvailable(String name)</code> should
+	 * evaluate to <code>true</code>. After addition, it should evaluate to
+	 * <code>false</code>.
+	 */
+	@Test
+	void testIsNameAvailable() {
+		AdminDAO adminDAO = DAOfactory.INSTANCE.getAdminDAO();
+		Admin testAdmin = new Admin();
+		testAdmin.setName("tester");
+		testAdmin.setPassword("1234");
+		assertTrue(adminDAO.isNameAvailable("tester"));
+		adminDAO.addAdmin(testAdmin);
+		assertFalse(adminDAO.isNameAvailable("tester"));
+		adminDAO.deleteEntity(testAdmin);
 	}
 
 }
